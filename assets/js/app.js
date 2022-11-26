6ca7eb1d784ec7e3c471c1f251340347d44cb146
Christian Samuel Seputra <stegenvak@gmail.com>
2022-11-26 22:54:35 +0700

    v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,17 @@ $('#play').click(function() {
     $( "#play" ).hide();
     $( ".score" ).show();
     // console.log('baaa');
+    
+    let i=0;
+    let x=0;
+    $('#player_kiri').text(i);
+    $('#player_kanan').text(x);
+
+    $('#score_kiri').click(function() {
+        $('#player_kiri').text(i++);
+    });
+
+    $('#score_kanan').click(function() {
+        $('#player_kanan').text(x++);
+    });
 });
---
+++ play.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/d508f9c7b5.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Wasit</title>
+</head>
+<body>
+    
+    <main>
+        <div class="container-home">
+            <div class="one mb-2">
+                <div class="kiri">
+                    <button class="btn btn-light btn-back"><i class="fa-solid fa-circle-arrow-left"></i></button>
+                </div>
+
+                <div class="tengah">
+                    <button class="btn btn-light btn-play" id="play"><i class="fa-solid fa-circle-play"></i></button>
+                    <div class="score">
+
+                        <div class="tim-kiri pt-4">
+                            <p>Madura United</p>
+                            <h1 id="player_kiri"></h1>
+                        </div>
+
+                        <div class="tengah pt-4">
+                            <h4>VS</h4>
+                        </div>
+
+                        <div class="tim-kanan pt-4">
+                            <p>Sumenep FC</p>
+                            <h1 style="padding-left: 40px" id="player_kanan"></h1>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="kanan">
+                    <button class="btn btn-light btn-warning"><i class="fa-solid fa-triangle-exclamation"></i></button>
+                </div>
+
+            </div>
+
+            <div class="two">
+                <div class="kiri">
+                    <div class="atas">
+                        <button class="btn btn-light"><i class="fa-solid fa-minus"></i> <img src="assets/img/cock.png" alt=""></button>
+                    </div>
+                    <div class="bawah">
+                        <button class="btn btn-light" id="score_kiri"><h1>SCORE</h1></button>
+                    </div>
+                </div>
+                
+                <div class="tengah">
+                    <div class="lapangan">
+                        <img src="assets/img/lapangan.jpg" alt="">
+                        <div class="player1-a">
+                            <h4>Samsudin</h4>
+                        </div>
+                        <div class="player1-b">
+                            <h4>Joko</h4>
+                        </div>
+                        <div class="player2-a">
+                            <h4>Sugiri</h4>
+                        </div>
+                        <div class="player2-b">
+                            <h4>Widodo</h4>
+                        </div>
+                    </div>
+
+                    <div class="skor">
+                        <table class="table table-bordered border-dark">
+                            <thead>
+                                <tr>
+                                    <td>Samsudin</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Joko</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Sugiri</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Widodo</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="kanan">
+                    <div class="atas">
+                        <button class="btn btn-light"><i class="fa-solid fa-plus"></i> <img src="assets/img/cock.png" alt=""></button>
+                    </div>
+                    <div class="bawah">
+                        <button class="btn btn-light" id="score_kanan"><h1>SCORE</h1></button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
