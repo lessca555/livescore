446f69a9ddcdff1fb655ed0b7bbfa6d9948b5474
Christian Samuel Seputra <stegenval@gmail.com>
2022-12-04 15:27:17 +0700

    passing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,50 @@
 const round_ov = new bootstrap.Modal('#round_over', {
     
 });
+//cock position
+let a1 = $('.1a');
+let a2 = $('.1b');
+let b1 = $('.2a');
+let b2 = $('.2b');
 
+//players
+// let p1a = $('h4').html("Samsudin");
+// let p1b = $('h4').html("Joko");
+// let p2a = $('h4').html("Sugiri");
+// let p2b = $('h4').html("Widodo");
+
+let x=1;
+let i=1;
+let current = -1;
+
+$('.tim_a1').click(function(){
+    $(a1).prepend(ball);
+    current = 1;
+});
+
+$('.tim_a2').click(function(){
+    $(a2).prepend(ball);
+    current = 1;
+});
+
+$('.tim_b1').click(function(){
+    $(b1).prepend(ball);
+    current = 2;
+});
+
+$('.tim_b2').click(function(){
+    $(b2).prepend(ball);
+    current = 2;
+});
+
+let ball = "<img src='assets/img/cock.png' alt=''>";
 $('.score').hide();
 
 $('.play').click(function() {
     $( "#play" ).hide();
     $( ".score" ).show();
     // console.log('baaa');
-    let x=1;
-    let i=1;
-    let current = -1;
+    
 
     function swapContent(a, b){
         tmp = $(a).html()
@@ -20,32 +54,34 @@ $('.play').click(function() {
 
     $('#score_kiri').click(function() {
         $('#player_kiri').val(i++);
-        swapContent('.player1-a', '.player1-b');
+        swapContent('.p1a', '.p1b')
+        swapContent(a1, a2)
         
         // console.log($('.1b').html(), $('.1a').html());
         if (current != 1){
             current = 1;
-            swapContent('.1b', '.2b');
-            swapContent('.1a', '.2a');
-        }else{
-            
+            swapContent(a1, b2);
+            swapContent(a2, b1);
         }
+        console.log(current);
     });
     
     
 
     $('#score_kanan').click(function() {
         $('#player_kanan').val(x++);
-        swapContent('.player2-a', '.player2-b');
+        swapContent('.p2a', '.p2b')
+        swapContent(b1, b2)
 
         if (current != 2){
             current = 2;
-            swapContent('.1a', '.2a');
-            swapContent('.1b', '.2b');
-        }else{
-            
+            swapContent(b1, a2);
+            swapContent(b2, a1);
         }
+        
+        console.log(current);
     });
+
 
     let cocks=1;
 
@@ -67,21 +103,5 @@ $('.tim-1').click(function(){
 $('.tim-2').click(function(){
     $('#tim-a').show();
     $('#tim-b').hide();
-});
-
-$('.tim_a1').click(function(){
-    $('.1a').prepend("<img src='assets/img/cock.png' alt=''>");
-});
-
-$('.tim_a2').click(function(){
-    $('.1b').prepend("<img src='assets/img/cock.png' alt=''>");
-});
-
-$('.tim_b1').click(function(){
-    $('.2a').prepend("<img src='assets/img/cock.png' alt=''>");
-});
-
-$('.tim_b2').click(function(){
-    $('.2a').prepend("<img src='assets/img/cock.png' alt=''>");
 });
 
